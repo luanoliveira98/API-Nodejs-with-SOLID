4b715e72062224bd6697cc81652602a980156d28
Luan Oliveira <luanoliveira17@hotmail.com>
2024-09-24 11:06:21 -0300

    test: add authenticate use case unit tests

--- src/use-cases/resgister.use-case.spec.ts
+++ src/use-cases/resgister.use-case.spec.ts
@@ -5,15 +5,15 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { UserAlreadyExistsError } from './errors/user-already-exists.error'
 
 describe('Register Use Case', () => {
-  let registerUseCase: RegisterUseCase
+  let sut: RegisterUseCase
 
   beforeEach(() => {
     const usersRepository = new InMemoryUsersRepository()
-    registerUseCase = new RegisterUseCase(usersRepository)
+    sut = new RegisterUseCase(usersRepository)
   })
 
   it('should be able to register', async () => {
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
@@ -23,7 +23,7 @@ describe('Register Use Case', () => {
   })
 
   it('should hash user password upon registration', async () => {
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
@@ -40,14 +40,14 @@ describe('Register Use Case', () => {
   it('should not be able to register with same email twice', async () => {
     const email = 'johndoe@example.com'
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: 'John Doe',
       email,
       password: '123456',
     })
 
     await expect(() =>
-      registerUseCase.execute({
+      sut.execute({
         name: 'John Doe',
         email,
         password: '123456',
