9ef7788eb4dd65e969c8b87d293f3c6b93becb5b
Luan Oliveira <luanoliveira17@hotmail.com>
2024-10-08 11:48:15 -0300

    feat: add refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { registerController } from './register.controller'
 import { authenticateController } from './authenticate.controller'
 import { profileController } from './profile.controller'
 import { verifyJwtMiddleware } from '@/http/middlewares/verify-jwt.middleware'
+import { refreshController } from './refresh.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
 
   app.post('/sessions', authenticateController)
+
+  app.patch('/token/refresh', refreshController)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwtMiddleware] }, profileController)
